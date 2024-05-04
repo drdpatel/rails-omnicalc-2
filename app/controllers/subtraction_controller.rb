544ff90b80a9a1a7024fb72ba98293f0877bdf19
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-05-04 16:57:48 +0000

    sub done

--- app/controllers/subtraction_controller.rb
+++ app/controllers/subtraction_controller.rb
@@ -3,12 +3,12 @@ class SubtractionController < ApplicationController
     render({ :template => "subtraction_templates/subtraction_form" })
   end
 
-  def subtract_these
+  def subtract
     @first_number = params.fetch("first_num").to_f
-    second_number = params.fetch("second_num").to_f
+    @second_number = params.fetch("second_num").to_f
 
-    @result =  @second_number - @first_number
-
+    @result =  @first_number - @second_number
+    
     render({ :template => "subtraction_templates/sub_results" })
   end
 end
